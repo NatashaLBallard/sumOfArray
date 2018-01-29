fada1765f9dde6c789d10aba7b1141cddbf6f3f4
NatashaLBallard <nbitcareer@gmail.com>
2018-01-29 16:55:40 -0500

    sum calculation and display works

--- src/main/java/com/sumofarray/demo/MainController.java
+++ src/main/java/com/sumofarray/demo/MainController.java
@@ -9,66 +9,22 @@ import java.util.Scanner;
 public class MainController {
 
     @RequestMapping("/")
-    public class sumofarray {
+    public String sumofarray () {
 
-    public  void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,8,9};
-        int sum = 0;
-       // for (int i=0; i<arr.length; i++);
-        //{
-          //  sum  += arr[i];
 
-        //}
-        System.out.println(sum + arr[i]);
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++)
+            {
+                  sum  += arr[i];
 
+            } return sum +"";
+
+
+        }
 
     }
 
-
-        //System.out.print("Enter length of array");
-        //Scanner numEntered = new Scanner(System.in);
-       // int arr[] = new int[numEntered];
-
-        //int n = 0;
-        // int sum = 0;
-
-        //System.out.print("Enter number elements");
-        //for (int i = 0; i < arr.length; i++) ;
-        //{
-        //    arr[Integer.valueOf(i)] = numEntered.nextInt();
-        //}
-        //int n = numEntered.nextInt();
-
-        //int sum = sumOfElements(arr);
-        //System.out.println("Sum of numbers" + sum);
-        //numEntered.close();
-    //}
-
-   // public int sumOfElements(int[] array) {
-
-        //int sum = 0;
-        //for (int i : array){
-       //     sum = sum + i;
-        //}
-        //return sum;
-  //  }
-
-
-
-
-
-         //int sum() {
-           // int sum = 0;
-
-            //for (int i = 0; i < arr.length; i++) {
-              //  sum += arr[i];
-
-
-                //System.out.println(sum);
-            }
-            //return sum;
-
-        }
 
 
 
