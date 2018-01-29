e26fc5dbabb3e8339dde3c1d236adcd9df21c8f0
NatashaLBallard <nbitcareer@gmail.com>
2018-01-29 16:19:41 -0500

    still experimenting

--- src/main/java/com/sumofarray/demo/MainController.java
+++ src/main/java/com/sumofarray/demo/MainController.java
@@ -1,21 +1,71 @@
 package com.sumofarray.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+@RestController
 public class MainController {
 
-    public class sumofarray
+    @RequestMapping("/")
+    public class sumofarray {
 
-    {
-        int arr[] = {1,3,6,9,12,15,18};
-
-        int sum = 0;
-        int i;
-
-        for (i = 0; i < arr.length; i++)
-            sum += arr[i];
-
-        return sum;
+    public void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9};
+        for (int i=0; i<arr.length; i++);
+        System.out.println();
 
 
 
     }
-}
+
+
+        //System.out.print("Enter length of array");
+        //Scanner numEntered = new Scanner(System.in);
+       // int arr[] = new int[numEntered];
+
+        //int n = 0;
+        // int sum = 0;
+
+        //System.out.print("Enter number elements");
+        //for (int i = 0; i < arr.length; i++) ;
+        //{
+        //    arr[Integer.valueOf(i)] = numEntered.nextInt();
+        //}
+        //int n = numEntered.nextInt();
+
+        //int sum = sumOfElements(arr);
+        //System.out.println("Sum of numbers" + sum);
+        //numEntered.close();
+    //}
+
+   // public int sumOfElements(int[] array) {
+
+        //int sum = 0;
+        //for (int i : array){
+       //     sum = sum + i;
+        //}
+        //return sum;
+  //  }
+
+
+
+
+
+         //int sum() {
+           // int sum = 0;
+
+            //for (int i = 0; i < arr.length; i++) {
+              //  sum += arr[i];
+
+
+                //System.out.println(sum);
+            }
+            //return sum;
+
+        }
+
+
+
+
